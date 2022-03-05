fbb6cceb67d9b95585bbfded8488800a870e4f7d
Andriy Kornatskyy <andriy.kornatskyy@live.com>
2022-03-05 20:15:30 +0200

    Added update time to product.

--- src/main/java/org/example/graphqldemo/core/Product.java
+++ src/main/java/org/example/graphqldemo/core/Product.java
@@ -1,7 +1,10 @@
 package org.example.graphqldemo.core;
 
+import java.time.Instant;
+
 public final class Product {
   public String id;
   public String type;
   public String name;
+  public Instant updateTime;
 }
